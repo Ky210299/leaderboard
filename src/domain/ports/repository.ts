b398f01eab4a32caba4bcf6b5a4b6434a728ab49
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-10-31 17:23:28 +0100

    implementing repository interface for persistency

--- src/domain/ports/repository.ts
+++ src/domain/ports/repository.ts
@@ -1,3 +1,27 @@
+import { Game, Player, Score } from "../DTOs";
+
+const UPDATE_SCORE_OPTIONS_TYPES = {
+	ADD: "add",
+	SUBSTRACT: "sub",
+	SET: "set",
+} as const;
+
+type UpdateScoreOptions =
+	| { type: typeof UPDATE_SCORE_OPTIONS_TYPES.ADD; value: number }
+	| { type: typeof UPDATE_SCORE_OPTIONS_TYPES.SUBSTRACT; value: number }
+	| { type: typeof UPDATE_SCORE_OPTIONS_TYPES.SET; value: number };
+
 export default interface Repository {
-	
+	addPlayer: () => Promise<Player>;
+	addGame: () => Promise<Game>;
+	updateScore: (
+		playerId: Player["id"],
+		gameId: Game["id"],
+		newScore: UpdateScoreOptions,
+	) => Promise<Score>;
+	findPlayer: (playerId: Player["id"]) => Promise<Player>;
+	findPlayerScores: (playerId: Player["id"]) => Promise<Score[]>;
+	findPlayerScoreByGame: (playerId: Player["id"], gameId: Game["id"]) => Promise<Score>;
+	deletePlayerById: (playerId: Player["id"]) => Promise<void>;
+	deleteGameById: (gameId: Game["id"]) => Promise<void>;
 }
