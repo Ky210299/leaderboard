47c6e163af76f52f26c3b695b14b9b82b22bc9dc
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-10-31 17:38:08 +0100

    implementing cacheService

--- src/domain/services/cacheService.ts
+++ src/domain/services/cacheService.ts
@@ -1,8 +1,17 @@
-import { CachePort } from "./ports/cache";
+import { Game } from "../DTOs";
+import CachePort from "../ports/cache";
 
 export default class CacheService {
 	private readonly cache: CachePort;
 	constructor(cacheAdapter: CachePort) {
 		this.cache = cacheAdapter;
+	}
+
+	public async saveLeaderboardByGameId(gameId: Game["id"]) {
+		return await this.cache.updateLeaderboardByGame(gameId);
+	}
+
+	public async getLeaderboardByGame(gameId: Game["id"]) {
+		return await this.cache.getLeaderboardByGame(gameId);
 	}
 }
