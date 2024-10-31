182a87d28a4168ac3ce01db5eea2c63f2bb76b23
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-10-31 17:34:56 +0100

    fix: parameter name on methods for caching changed from "game" to "gameId"

--- src/domain/ports/cache.ts
+++ src/domain/ports/cache.ts
@@ -1,6 +1,6 @@
 import { Game, Leaderboard } from "../DTOs";
 
 export default interface CachePort {
-	getLeaderboardByGame: (game: Game["id"]) => Promise<Leaderboard>;
-	updateLeaderboardByGame: (game: Game["id"]) => Promise<void>;
+	getLeaderboardByGame: (gameId: Game["id"]) => Promise<Leaderboard>;
+	updateLeaderboardByGame: (gameId: Game["id"]) => Promise<void>;
 }
