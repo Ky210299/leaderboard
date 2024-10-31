3338d3620d8a4afb41ab9e4ed597378c3def27d5
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-10-31 16:44:20 +0100

    implementing cache port interface

--- src/domain/ports/cache.ts
+++ src/domain/ports/cache.ts
@@ -1,1 +1,6 @@
-export default interface CachePort {}
+import { Leaderboard } from "../DTOs";
+
+export default interface CachePort {
+	getLeaderboard: () => Promise<Leaderboard>;
+	updateLeaderboard: () => Promise<void>;
+}
