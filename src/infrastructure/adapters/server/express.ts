107af5a5e11c2602788752c2e46ccdf5baf3e8ce
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-11-26 15:32:25 +0100

    implementing ServerPort interface on Express infrastructure

--- src/infrastructure/adapters/server/express.ts
+++ src/infrastructure/adapters/server/express.ts
@@ -3,4 +3,23 @@ import app from "./server";
 
 export default class ExpressServer implements ServerPort {
 	private readonly server = app;
+	private appInstance: ReturnType<typeof app.listen> | null;
+	constructor() {
+		this.appInstance = null;
+	}
+
+	public start(port: number) {
+		const appInstance = this.appInstance;
+		if (appInstance !== null) return;
+		this.appInstance = app.listen(port, () => {
+			console.log(`Aplication running on port: ${port}`);
+		});
+	}
+	public stop() {
+		const appInstance = this.appInstance;
+		if (appInstance != null) {
+			this.appInstance = null;
+			appInstance.close();
+		}
+	}
 }
