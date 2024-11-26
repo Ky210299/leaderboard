2d2c2317c36e8c778b437b6ae589544c6cce8bc0
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-11-26 15:07:55 +0100

    implementing repository service

--- src/domain/services/persistenceService.ts
+++ src/domain/services/persistenceService.ts
@@ -1,4 +1,4 @@
-import { Game, Player } from "../DTOs";
+import { Activity, Leaderboard, Participant, Score } from "../DTOs";
 import Repository, { UPDATE_SCORE_OPTIONS_TYPES } from "../ports/repository";
 
 export default class PersistencyService {
@@ -8,52 +8,52 @@ export default class PersistencyService {
 		this.persistency = persistency;
 	}
 
-	public addPlayer(player: Omit<Player, "id">) {
-		return this.persistency.addPlayer(player);
+	public async addParticipant(participant: Participant) {
+		if (!participant.id || !participant.name) throw new Error("Invalid participant data");
+		return this.persistency.addParticipant(participant);
 	}
 
-	public addGame(game: Omit<Game, "id">) {
-		return this.persistency.addGame(game);
+	public async addToScore(
+		participantId: Participant["id"],
+		activity: Activity,
+		score: Score["value"],
+	) {
+		if (!score || score === 0) return;
+		this.persistency.sumToScore(participantId, activity, score);
 	}
 
-	public addToScore(playerId: Player["id"], gameId: Game["id"], amount: number) {
-		return this.persistency.updateScore(playerId, gameId, {
-			type: UPDATE_SCORE_OPTIONS_TYPES.ADD,
-			value: amount,
-		});
+	public async subtractToScore(
+		participantId: Participant["id"],
+		activity: Activity,
+		score: Score["value"],
+	) {
+		if (!score || score === 0) return;
+
+		const existParticipant = await this.persistency.findParticipantById(participantId);
+		if (existParticipant == null) throw new Error("Participant not found");
+		this.persistency.sumToScore(participantId, activity, -score);
 	}
 
-	public subtractToScore(playerId: Player["id"], gameId: Game["id"], amount: number) {
-		return this.persistency.updateScore(playerId, gameId, {
-			type: UPDATE_SCORE_OPTIONS_TYPES.SUBTRACT,
-			value: amount,
-		});
+	public async setScore(
+		participantId: Participant["id"],
+		activity: Activity,
+		score: Score["value"],
+	) {
+		const existParticipant = await this.persistency.findParticipantById(participantId);
+		if (existParticipant == null) throw new Error("Participant not found");
+
+		this.persistency.setScore(participantId, activity, score);
 	}
 
-	public setScore(playerId: Player["id"], gameId: Game["id"], amount: number) {
-		return this.persistency.updateScore(playerId, gameId, {
-			type: UPDATE_SCORE_OPTIONS_TYPES.SET,
-			value: amount,
-		});
+	public async findParticipantById(participantId: Participant["id"]) {
+		return this.persistency.findParticipantById(participantId);
 	}
 
-	public findPlayerById(playerId: Player["id"]) {
-		return this.persistency.findPlayer(playerId);
+	public async findAllParticipants() {
+		return this.persistency.findAllParticipants();
 	}
 
-	public findScoresOfPlayerById(playerId: Player["id"]) {
-		return this.persistency.findPlayerScores(playerId);
-	}
-
-	public findScoreOfPlayerInGame(playerId: Player["id"], gameId: Game["id"]) {
-		return this.persistency.findPlayerScoreByGame(playerId, gameId);
-	}
-
-	public deleteGameById(gameId: Game["id"]) {
-		return this.persistency.deleteGameById(gameId);
-	}
-
-	public deletePlayerById(playerId: Player["id"]) {
-		return this.persistency.deletePlayerById(playerId);
+	public async deleteParticipant(participantId: Participant["id"]) {
+		this.persistency.deleteParticipantById(participantId);
 	}
 }
