033cec137756fab16919b3c478374cbc7ea4caae
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-11-26 20:43:09 +0100

    Squashed commit of the following: Created UserCase Class and CreateParticipant class with the method execute
    commit b9dc1af943d8dce2d9b2c2e0a9b8b0b527c41547
    Author: Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
    Date:   Tue Nov 26 20:39:22 2024 +0100
    
        Added UserCase class for parent class of all uses cases.
        Created CreateParticipant class with the execute method

--- src/application/useCases.ts
+++ src/application/useCases.ts
@@ -1,17 +1,54 @@
 import { CacheService, PersistencyService, ServerService } from "../domain";
+import { Participant } from "../domain/DTOs";
 
-export default class UseCasesExecutor {
-	private readonly persistence: PersistencyService;
-	private readonly cache: CacheService;
-	private readonly server: ServerService;
+class UseCases {
+	protected readonly persistence: PersistencyService | null;
+	protected readonly cache: CacheService | null;
+	protected readonly server: ServerService | null;
 
 	constructor(
-		persistenceService: PersistencyService,
-		cacheService: CacheService,
-		serverService: ServerService,
+		persistenceService: PersistencyService | null,
+		cacheService: CacheService | null,
+		serverService: ServerService | null,
 	) {
-		this.persistence = persistenceService;
-		this.cache = cacheService;
-		this.server = serverService;
+		this.persistence = persistenceService || null;
+		this.cache = cacheService || null;
+		this.server = serverService || null;
+	}
+
+	protected static isValidPersistencyServiceInstance(instance: any) {
+		return instance != null && instance instanceof PersistencyService;
+	}
+	protected static isValidCacheServiceInstance(instance: any) {
+		return instance != null && instance instanceof CacheService;
+	}
+	protected static isValidServerServiceInstance(instance: any) {
+		return instance != null && instance instanceof ServerService;
+	}
+
+	protected havePersistency() {
+		return this.persistence != null;
+	}
+	protected haveCache() {
+		return this.cache != null;
+	}
+	protected haveServer() {
+		return this.server != null;
+	}
+}
+
+export class CreateParticipant extends UseCases {
+	constructor(persistenceService: PersistencyService) {
+		if (UseCases.isValidPersistencyServiceInstance(persistenceService) === false) {
+			throw new Error('The "Create Participant Use Case" need a valid persistency instance');
+		}
+		super(persistenceService, null, null);
+	}
+
+	async execute(participant: Participant) {
+		if (participant == null) throw new Error("Invalid participant data");
+		const { id, name } = participant;
+		if (id == null || !name) throw new Error("Invalid participant data");
+		await this.persistence?.addParticipant(participant);
 	}
 }
