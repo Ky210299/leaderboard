fe22a00810471fb3f16a826dbfb03111e7fb60b4
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-12-05 13:19:14 +0100

    working on documentation with swagger

--- src/infrastructure/adapters/server/server.ts
+++ src/infrastructure/adapters/server/server.ts
@@ -3,6 +3,21 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import { leaderboardRouter } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+const jSDocOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Leaderboard",
+            version: "1.0.0",
+        },
+    },
+    apis: ["./routesDoc"],
+};
+
+const swaggerOptions = swaggerJSDoc(jSDocOptions);
 
 const app = express();
 
@@ -11,6 +26,8 @@ app.use(json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 // ROUTES
 app.get("/live", async (req: Request, res: Response) => {
